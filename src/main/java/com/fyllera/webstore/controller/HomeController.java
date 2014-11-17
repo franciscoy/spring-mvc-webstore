d5942b24caa36e3304abf488b9e962035013ff2d
franciscoy <francisco.yllera@gmail.com>
2014-11-16 23:02:28 -0300

    Adding resources .png

--- src/main/java/com/fyllera/webstore/controller/HomeController.java
+++ src/main/java/com/fyllera/webstore/controller/HomeController.java
@@ -10,6 +10,11 @@ public class HomeController {
 	public String welcome(Model model) {
 		model.addAttribute("greeting", "Welcome to Web Store!");
 		model.addAttribute("tagline", "The one and only amazing webstore");
+		return "forward:/welcome/greeting";
+	}
+
+	@RequestMapping("/welcome/greeting")
+    public String greeting() {
 		return "welcome";
 	}
 }
